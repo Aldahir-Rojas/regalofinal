671d9bfd4dd89b03d75bc6c21c143900879de1b0
Aldahir-Rojas <aldahir080698@gmail.com>
2020-10-25 15:57:01 -0500

    tercero

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Informacion} from '../interfaces/info-pagina';
+import { Informacion } from '../interfaces/info-pagina';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
